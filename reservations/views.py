9de58209ae0af5666c432868fc9b12b14c569759
MehedyHridoy <mehedyhridoy6@gmail.com>
2023-10-25 11:18:45 +0600

    Commit by MEHEDIIII

--- reservations/views.py
+++ reservations/views.py
@@ -1,24 +1,47 @@
-# Create your views here.
-from django.contrib.auth.models import User
+# # Create your views here.
+# from django.contrib.auth.models import User
+# from django.http import HttpResponse
+# from django.shortcuts import render
+# from . import forms
+#
+#
+# def reserve_table(request):
+#     if request.method == 'GET':
+#
+#         form = forms.TableReservationForm(request.GET)
+#
+#         if form.is_valid():
+#             form.save()
+#
+#             return HttpResponse("Table Reserved Successfully")
+#
+#     else:
+#         form = forms.TableReservationForm()
+#
+#     return render(request, 'form.html', {
+#         'form': form
+#     })
+#
+
+
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
-from .import forms
+from .forms import TableReservationForm  # Import your form
 
 
+@login_required
 def reserve_table(request):
-
-    if request.method == 'GET':
-
-        form = forms.TableReservationForm(request.GET)
+    if request.method == 'POST':
+        form = TableReservationForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            reservation = form.save(commit=False)  # Create a reservation object without saving
+            reservation.user = request.user  # Associate the reservation with the logged-in user
+            reservation.save()  # Now save to the database
 
             return HttpResponse("Table Reserved Successfully")
-
     else:
-        form = forms.TableReservationForm()
+        form = TableReservationForm()
 
-    return render(request, 'form.html', {
-        'form':form
-    })
+    return render(request, 'form.html', {'form': form})
