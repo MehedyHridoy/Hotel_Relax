0a213c3936859adfc8dd8033fd400b3b05a596a5
MehedyHridoy <mehedyhridoy6@gmail.com>
2023-10-26 17:22:09 +0600

    Commit by MEHEDIIII

---
+++ foods/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Food(models.Model):
+    name = models.CharField(max_length=120)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.URLField()
+
+    def __str__(self):
+        return 'Food Name: ' + self.name
